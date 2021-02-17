addbf781328e0c4e1b9a5686c616b4048433d44b
leejanghyun1 <63733215+leejanghyun1@users.noreply.github.com>
2021-02-17 15:41:06 +0900

    Last Commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,16 +10,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject Ai;
 
-    //플레이어와 Ai의 선택을 int로 표시 : 0은 협력, 1은 배신
+    //플레이어와 Ai의 선택을 int로 표시 : 0은 협력, 1은 배신, 2은 클릭하지 않음 
     private int Player_Select;
     private int Ai_Select;
 
     //소지한 알의 수
-    public int Player_EggCount;
-    public int Ai_EggCount;
+    private int Player_EggCount;
+    private int Ai_EggCount;
 
     //한 판에 거는 알의 수
-    public int Bet = 5;
+    private int Bet = 5;
 
     //소지한 알의 수 표시
     public Text Player_Egg;
@@ -29,13 +29,9 @@ public class GameManager : MonoBehaviour
     public Text TimeCount;
     private float Timer;
 
-    //Animator 선언
-    private Animator player_anim;
-    private Animator Ai_anim;
-
     //라운드 수
     private int Round = 1;
-    public int maxRound = 16;
+    private int maxRound = 11;
     public Text RoundText;
     public GameObject Round_Panel;
 
@@ -46,11 +42,8 @@ public class GameManager : MonoBehaviour
     //결과를 넣는 리스트
     private List<int> ResultList = new List<int>();
 
-    //결과 리스트 뒤집기
-    private List<int> ReverseList = new List<int>();
-
     //플레이어가 버튼을 눌렀는지 아닌지
-    [SerializeField] private bool isClick = false;
+    private bool isClick = false;
 
     //배신, 협력 버튼과 버튼 안의 텍스트
     public Button Greed_Btn;
@@ -84,12 +77,34 @@ public class GameManager : MonoBehaviour
     // 메인 패널
     public GameObject MainPanel;
 
+    // "Game Start"
     public Text GameStartText;
+
+    // Choice Ai
+    private int RandomAiNum;
+
+    // count how many player choose Alone or With
+    private int numAlone = 0;
+    private int numWith = 0;
+
+    // Betting Effect
+    public GameObject playerEgg;
+    public GameObject aiEgg;
+    Vector2 playerEggStart = new Vector2(-2f, -2.21f);
+    Vector2 aiEggStart = new Vector2(2f, -2.21f);
+    Vector2 eggPoint = new Vector2(0f, -2.81f);
+    private float speed = 0.5f;
+    public GameObject playerEffect;
+    public GameObject aiEffect;
 
     void Start()
     {
-        GameOverPanel.SetActive(false);
+        // Select Ai Number
+        RandomAiNum = Random.Range(0, 6);
+
+        // Bgm Volume
         Bgm.volume = 0.4f;
+
         //시간 제한은 10초
         Timer = 21f;
 
@@ -97,20 +112,16 @@ public class GameManager : MonoBehaviour
         Player_EggCount = 100;
         Ai_EggCount = 100;
 
-        //애니메이션 속도 조절
-        player_anim = Player.GetComponent<Animator>();
-        Ai_anim = Ai.GetComponent<Animator>();
-        player_anim.speed = 0.5f;
-        Ai_anim.speed = 0.5f;
-
+        // Text how many you bet
         BetText.text = $"You Bet {Bet} Eggs";
     }
 
     void Update()
     {
         //게임 오버
-        if (Round == maxRound || Player_EggCount <= 0 || Ai_EggCount <= 0)
+        if (Round == maxRound || Player_EggCount <= 0 || Ai_EggCount <= 0) // round over, or Egg <= 0
         {
+            Player_EggCount = 0;
             StartCoroutine(GameOver());
         }
         else
@@ -119,7 +130,7 @@ public class GameManager : MonoBehaviour
             Timer = Timer - Time.deltaTime;
             TimeCount.text = $"{(int)Timer}";
             //destroy Game Start text 
-            if(Timer <= 19)
+            if (Timer <= 19)
             {
                 Destroy(GameStartText);
             }
@@ -132,6 +143,11 @@ public class GameManager : MonoBehaviour
                 Greed_Btn.interactable = true;
                 Co_Btn.interactable = true;
                 Round++;
+                playerEgg.SetActive(true);
+                aiEgg.SetActive(true);
+                ResultList.Add(Player_Select);
+                playerEgg.transform.position = playerEggStart;
+                aiEgg.transform.position = aiEggStart;
             }
 
             //알 갯수 텍스트
@@ -140,6 +156,12 @@ public class GameManager : MonoBehaviour
 
             //라운드 텍스트
             RoundText.text = $"Round {Round}";
+        }
+
+        if (isClick)
+        {
+            playerEgg.transform.position = Vector2.MoveTowards(playerEgg.transform.position, eggPoint, speed);
+            aiEgg.transform.position = Vector2.MoveTowards(aiEgg.transform.position, eggPoint, speed);
         }
     }
 
@@ -155,13 +177,14 @@ public class GameManager : MonoBehaviour
     {
         Btn();
 
-        ResultList.Add(1);
         GreedCount++;
 
         PlayerResultText.text = "Alone";
 
+        numAlone++;
+
         Player_Select = 1;
-        AiSelect();
+        ChoiceAi(RandomAiNum);
     }
 
     //협력 버튼
@@ -169,51 +192,14 @@ public class GameManager : MonoBehaviour
     {
         Btn();
 
-        ResultList.Add(0);
         CoCount++;
 
         PlayerResultText.text = "With";
 
+        numWith++;
+
         Player_Select = 0;
-        AiSelect();
-    }
-
-    //Ai의 배신 혹은 협력 선택 함수
-    void AiSelect()
-    {
-        //플레이어의 선택을 집어넣은 리스트를 뒤집어놓은 리스트를 생성
-        ResultList.Reverse();
-        for(int i = 0; i < ResultList.Count; i++)
-        {
-            ReverseList.Add(ResultList[i]);
-        }
-        ResultList.Reverse(); //원래대로 되돌리기
-
-        //라운드 1~5
-        if (Round >= 1 && Round <= 5)
-        {
-            Ai_Select = Random.Range(0, 2);
-        }
-        //라운드 6~n
-        else
-        {
-            if (GreedCount == CoCount || Mathf.Abs(GreedCount - CoCount) < 3) { 
-                Ai_Select = Random.Range(0, 2); //배신 협력 횟수가 같거나 서로의 차가 3 이하일 때 랜덤으로 부여한다.
-            }
-            else if(Mathf.Abs(GreedCount - CoCount) >= 3)
-            {
-                if (GreedCount > CoCount)
-                {
-                    Ai_Select = 1; //플레이어가 배신한 횟수가 3번 더 많을 시에 배신을 선택
-                }
-                else if (GreedCount < CoCount)
-                {
-                    Ai_Select = 0; //플레이어가 협력한 횟수가 3번 더 많을 시에 협력을 선택
-                }
-            }
-        }
-        if (Ai_Select == 0) AiResultText.text = "With";
-        else AiResultText.text = "Alone";
+        ChoiceAi(RandomAiNum);
     }
 
     // 승 패 결정 함수
@@ -227,6 +213,7 @@ public class GameManager : MonoBehaviour
             PlayerCountText.text = $"-{2 * Bet}";
             AiCountText.text = "";
             audioSources[1].Play();
+            Player_Select = 2;
         }
         else
         {
@@ -240,6 +227,8 @@ public class GameManager : MonoBehaviour
                     PlayerCountText.text = $"+{2 * Bet}";
                     AiCountText.text = $"+{2 * Bet}";
                     audioSources[0].Play();
+                    playerEffect.SetActive(true);
+                    aiEffect.SetActive(true);
                 }
                 else
                 {
@@ -261,6 +250,7 @@ public class GameManager : MonoBehaviour
                     PlayerCountText.text = $"+{4 * Bet}";
                     AiCountText.text = $"-{Bet}";
                     audioSources[0].Play();
+                    playerEffect.SetActive(true);
                 }
                 else
                 {
@@ -270,14 +260,15 @@ public class GameManager : MonoBehaviour
                     PlayerCountText.text = $"-{Bet}";
                     AiCountText.text = $"+{4 * Bet}";
                     audioSources[1].Play();
+                    aiEffect.SetActive(true);
                 }
             }
         }
-        ReverseList.Clear(); //뒤집어 놓은 리스트 다시 비우기
         Timer = 16f; //타이머 리셋
         isClick = false; //클릭 여부를 다시 false로 바꿈
         StartCoroutine(ResultTextPanelOn()); //결과창 띄우기
         StartCoroutine(CountPanelText()); // + - 띄우기
+        StartCoroutine(OffEffect()); // 이펙트 띄우기
     }
 
     //걸은 알 갯수 
@@ -304,6 +295,13 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(2f);
         PlayerCountPanel.SetActive(false);
         AiCountPanel.SetActive(false);
+    }
+
+    IEnumerator OffEffect()
+    {
+        yield return new WaitForSeconds(0.5f);
+        playerEffect.SetActive(false);
+        aiEffect.SetActive(false);
     }
 
     //배신 협력 버튼 공통사항
@@ -345,5 +343,149 @@ public class GameManager : MonoBehaviour
         GameOverPanel.SetActive(true);
     }
 
-    
+    // RanAiNum에서 랜덤으로 선정된 int형에 따라 다른 Select 함수를 돌린다.
+    void ChoiceAi(int ranNum)
+    {
+        switch (ranNum)
+        {
+            case 0:
+                AiSelect_0();
+                break;
+            case 1:
+                AiSelect_1();
+                break;
+            case 2:
+                AiSelect_2();
+                break;
+            case 3:
+                AiSelect_3();
+                break;
+            case 4:
+                AiSelect_4();
+                break;
+            case 5:
+                AiSelect_5();
+                break;
+        }
+    }
+
+    //Ai의 배신 혹은 협력 선택 함수
+
+    // normal : basic algorithm
+    void AiSelect_0()
+    {
+
+        //라운드 1~5
+        if (Round >= 1 && Round <= 3)
+        {
+            Ai_Select = Random.Range(0, 2);
+        }
+        //라운드 6~n
+        else
+        {
+            if (GreedCount > CoCount)
+            {
+                Ai_Select = 1; //플레이어가 배신한 횟수가 더 많을 시에 배신을 선택
+            }
+            else if (GreedCount < CoCount)
+            {
+                Ai_Select = 0; //플레이어가 협력한 횟수가 더 많을 시에 협력을 선택
+            }
+            else if (GreedCount == CoCount)
+            {
+                Ai_Select = Random.Range(0, 2);
+            }
+        }
+        if (Ai_Select == 0) AiResultText.text = "With";
+        else AiResultText.text = "Alone";
+    }
+
+    // joker : only random select
+    void AiSelect_1()
+    {
+        Ai_Select = Random.Range(0, 2);
+        if (Ai_Select == 0) AiResultText.text = "With";
+        else AiResultText.text = "Alone";
+    }
+
+    // punishment : Alone -> Select Alone, With -> Select With
+    void AiSelect_2()
+    {
+        if (Round == 1)
+        {
+            Ai_Select = Random.Range(0, 2);
+        }
+        else
+        {
+            if (ResultList[ResultList.Count-1] == 2) // 플레이어가 전 라운드에 아무것도 누르지 않았다면 Ai는 현 라운드에 배신을 선택한다.
+            {
+                Ai_Select = 1;
+            }
+            else
+            {
+                Ai_Select = ResultList[ResultList.Count - 1];
+            }
+        }
+        if (Ai_Select == 0) AiResultText.text = "With";
+        else AiResultText.text = "Alone";
+    }
+
+    // Water ghost : Alone >= 3 -> Only Select Alone
+    void AiSelect_3()
+    {
+        if(numAlone >= 4) // 배신한 횟수가 3회 넘어갈 시 무조건 배신만 선택
+        {
+            Ai_Select = 1;
+        }
+        else
+        {
+            Ai_Select = Random.Range(0, 2); // 3회 이하면 랜덤 
+        }
+        if (Ai_Select == 0) AiResultText.text = "With";
+        else AiResultText.text = "Alone";
+    }
+
+    // Engel : With >= 3 -> Only Select With
+    void AiSelect_4()
+    {
+        if(numWith >= 4) // 협력한 횟수가 3회 넘어갈 시 무조건 협력만 선택
+        {
+            Ai_Select = 0;
+        }
+        else
+        {
+            Ai_Select = Random.Range(0, 2); // 3회 이하면 랜덤
+        }
+        if (Ai_Select == 0) AiResultText.text = "With";
+        else AiResultText.text = "Alone";
+    }
+
+    // Observer : Alone -> With -> Alone -> With : if Alone more, select alone.
+    void AiSelect_5()
+    {
+        if(Round <= 4)
+        {
+            if(Round % 2 == 0)
+            {
+                Ai_Select = 0;
+            }
+            else
+            {
+                Ai_Select = 1;
+            }
+        }
+        else
+        {
+            if(numAlone >= numWith)
+            {
+                Ai_Select = 1;
+            }
+            else 
+            {
+                Ai_Select = 0;
+            }
+        }
+        if (Ai_Select == 0) AiResultText.text = "With";
+        else AiResultText.text = "Alone";
+    }
 }
